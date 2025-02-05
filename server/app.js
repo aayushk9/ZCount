b26ded5aacf448396466a50747fc3d6677f5559a
aayushk9 <ayushkokate1@gmail.com>
2025-02-05 20:59:10 +0530

    fix: correct dirname

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public")));   
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
